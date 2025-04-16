f14a43d188057123f8c1415f00fb0588489de787
Negin-code <iamneginasem@gmail.com>
2025-04-16 07:29:25 -0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  base: '/', 
-  plugins: [tailwindcss()],
+  base: '/',
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          icons: ['react-icons'],
+          swiper: ['swiper', 'swiper/react']
+        }
+      }
+    }
+  }
 })
